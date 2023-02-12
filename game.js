a725bd42d6ea715c1d2c943ba65a6b3419c209a6
LiudmilaLV <lisikhina.lv@gmail.com>
2023-02-12 14:30:28 +0700

    add a function for playing one round

--- game.js
+++ game.js
@@ -1,7 +1,36 @@
-const options = ["Rock", "Pape", "Scissors"]
+document.addEventListener("DOMContentLoaded", () => {
+    const options = ["Rock", "Paper", "Scissors"]
 
-function getComputerChoice(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-};
-console.log(getComputerChoice(options));
+    function getComputerChoice(arr) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        return arr[randomIndex];
+    };
+
+    function playRound(you, opponent) {
+        if (you === opponent) {
+            return ("It's a tie!");
+        } else {
+            if ((you === "Rock" && opponent === "Scissors")
+            || (you === "Scissors" && opponent === "Paper")
+            || (you === "Paper" && opponent === "Rock")) {
+                return (`You win! ${you} beats ${opponent}!`);
+            } else {
+                return (`You lose! ${opponent} beats ${you}!`);
+            }
+        }
+    };
+
+    console.log("Hi! Let\'s play 'Rock, Paper, Scissors!'");
+
+    let word = "";
+    let playerSelection = "";
+    while (!(options.includes(playerSelection))) {
+        console.log("Please, type Rock, Paper or Scissors!");
+        word = prompt("Your choice: ");
+        playerSelection = word.charAt(0).toUpperCase()+ word.slice(1);
+    };
+
+    const computerSelection = getComputerChoice(options);
+    console.log(playRound(playerSelection, computerSelection));
+
+});
